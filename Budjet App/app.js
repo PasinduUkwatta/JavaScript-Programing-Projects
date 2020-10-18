01d82a58ee7d5e66ba6145f0449c1b4df8d9a058
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-10-18 23:08:25 +0530

    Budjet App

---
+++ Budjet App/app.js
@@ -0,0 +1,12 @@
+var budjetController =(function (){
+   var x=23
+    var add =function(a){
+       return x+a
+    }
+
+    return{
+       publicTest :function(b){
+          console.log(add(b))
+       }
+    }
+})()
