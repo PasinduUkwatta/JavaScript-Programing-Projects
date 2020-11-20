6fb407eff93926d8d3c0b140ff0653dc412e7a0c
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2020-11-20 16:23:33 +0530

    Day 1

---
+++ Assignments.js
@@ -0,0 +1,20 @@
+/*
+
+let country = "Sri Lanka"
+let continent = "Asia"
+let population = 200000000
+
+console.log(country)
+console.log(continent)
+console.log(population)
+
+let isIsland = true
+let language
+
+console.log(typeof (isIsland))
+console.log(typeof (population))
+console.log(typeof (country))
+console.log(typeof (language))
+
+*/
+
--- script.js
+++ script.js
@@ -1,7 +1,63 @@
+/*
 
-let js = "amazing"
-if (js === "amazing") {
-    alert("js is fun")
-}
+console.log("pasindu")
+console.log(10 + 65)
 
-console.log(43 + 8 + 23 - 8)
+
+let firstName = "deshan"
+
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+let myFirstJob = "Programmer"
+let myCurrentJob = "Teacher"
+
+console.log(firstName + myFirstJob + myCurrentJob)
+*/
+
+/*console.log(true)
+
+let javaScriptIsFun = true
+
+console.log(javaScriptIsFun)
+
+console.log(typeof (javaScriptIsFun))
+console.log(typeof (23))
+console.log(typeof ("pasindu"))
+
+
+javaScriptIsFun = "Yes !"
+
+console.log(javaScriptIsFun)
+console.log(typeof (javaScriptIsFun))
+
+let name
+
+console.log(name)
+console.log(typeof (name))
+
+
+name = "apple"
+
+console.log(name)
+console.log(typeof (name))
+
+
+console.log(typeof (null))
+
+*/
+//this can change
+let age = 30
+age = 31
+
+//const variable cannot re assign --immutable
+const birthYear = 1996
+
+var job = "programmer"
+
+job = "teacher"
+
+lastName = "thiwanka"
+
+console.log(lastName)
