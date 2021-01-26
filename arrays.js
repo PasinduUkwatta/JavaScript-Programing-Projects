6514fb00dfa5db5b175ee81a7223d09b9128cc12
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-01-26 21:10:11 +0530

    Array Practise

--- arrays.js
+++ arrays.js
@@ -1,37 +1,169 @@
-var names =['amal','kamal','nimal']
+// console.log("welcome to the array coding");
 
-console.log(names)
+// class Player {
+//   constructor(name, type) {
+//     this.name = name;
+//     this.type = type;
+//   }
 
-var marks =[10,2,0,30,3,0,2]
+//   introduce() {
+//     console.log(`Hello im ${this.name}, my type is ${this.type}`);
+//   }
+// }
 
-console.log(marks)
-console.log(marks.length)
+// class Wizard extends Player {
+//   constructor(name, type) {
+//     super(name, type);
+//     console.log("wizard", this);
+//   }
 
-names[3]='sunil'
+//   play() {
+//     console.log(`im ${this.type}`);
+//   }
+// }
 
-console.log(names)
+// const wizard1 = new Wizard("Pasindu", "Attack");
+// const wizard2 = new Wizard("Thiwanka", "Medic");
 
-//we can  use lenght method to add the elements to the last index
-marks[marks.length]= 100
-console.log(marks)
+// wizard1.introduce();
+// wizard2.introduce();
+// wizard1.play();
+// wizard2.play();
 
-var john = ['john','Smith',1990,'teacher',false]
+// const a = [4, 5, 3, 9, 0, 1];
+// console.log(a.sort());
 
-john.push('blue')
+// class myArray {
+//   constructor() {
+//     this.length = 0;
+//     this.data = {};
+//   }
 
-//this will add the element to the 0 index
-john.unshift('mr')
+//   get(index) {
+//     return this.data[index];
+//   }
 
-console.log(john)
+//   push(item) {
+//     this.data[this.length] = item;
+//     this.length++;
+//     return this.length;
+//   }
 
-//this will remove the last element of hte array
-john.pop()
-console.log(john)
+//   pop() {
+//     const lastItem = this.data[this.length - 1];
+//     delete this.data[this.length - 1];
+//     this.length--;
+//     return lastItem;
+//   }
 
-console.log(john.indexOf(1996))
+//   delete(index) {
+//     const item = this.data[index];
+//     this.shiftItems(index);
+//   }
 
-//we can  check about the availabilty of the array
-//if it is not available ,then we get -1
-var isDesigner =john.indexOf('designer')===-1 ?'john is not a designer':'john is a designer'
+//   shiftItems(index) {
+//     for (let i = index; i < this.length - 1; i++) {
+//       this.data[i] = this.data[i + 1];
+//     }
 
-console.log(isDesigner)
+//     delete this.data[this.length - 1];
+//     this.length--;
+//   }
+// }
+
+// const newArray = new myArray();
+// console.log(newArray.get[0]);
+// newArray.push("hi");
+// newArray.push("you");
+// console.log(newArray);
+
+// newArray.pop();
+// console.log(newArray);
+
+// newArray.delete(1);
+// newArray.push("call");
+// newArray.push("me");
+
+// console.log(newArray);
+
+// //reverse a string
+
+// function reverseString(str) {
+//   if (!str || str.length < 2 || typeof str !== "string") {
+//     return "Please enter corret input";
+//   } else {
+//     const backwards = [];
+//     const totalItems = str.length - 1;
+
+//     for (let i = totalItems; i >= 0; i--) {
+//       backwards.push(str[i]);
+//     }
+//     console.log(backwards);
+
+//     return backwards.join("");
+//   }
+// }
+
+// console.log(reverseString("Hi my name is pasindu"));
+
+// function reverse2(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// const reverse3 = (str) => [...str].reverse().join("");
+
+// console.log(reverse2("HI my name is thiwnaka"));
+
+// console.log(reverse3("HI my name is deshan"));
+
+let array1 = [0, 3, 4, 31];
+let array2 = [4, 6, 30];
+
+// function mergeSoratedArray(arr1, arr2) {
+//   let sortArr1 = arr1.sort();
+//   let sortArr2 = arr2.sort();
+
+//   while (
+//     sortArr1Index === arr1.length - 1 &&
+//     sortArr2Index === arr2.length - 1
+//   ) {
+//     let sortArr1Index = 0;
+//     let sortArr2Index = 0;
+//     let mergeArray = [];
+//     if (sortArr1Index > sortArr2Index) {
+//       mergeArray.push(sortArr2[sortArr2Index]);
+//       sortArr2Index++;
+//       console.log(sortArr2Index);
+//     } else if (sortArr1Index < sortArr2Index) {
+//       mergeArray.push(sortArr1[sortArr1Index]);
+//       sortArr1Index++;
+//       console.log(sortArr1Index);
+//     }
+//     console.log(mergeArray);
+//   }
+// }
+
+// mergeSoratedArray(array1, array2);
+
+function meargeSortedArray(arr1, arr2) {
+  let sortArr1 = arr1.sort();
+  let sortArr2 = arr2.sort();
+  let meargeArray = [];
+  let arr1Index = 0;
+  let arr1Length = sortArr1.length - 1;
+  let arr2Index = 0;
+  let arr2Length = sortArr2.length - 1;
+  while (arr1Index === arr1Length && arr2Index === arr2Length) {
+    if (sortArr1[arr1Index] > sortArr2[arr2Index]) {
+      meargeArray.push(sortArr2[arr2Index]);
+      arr2Index++;
+    } else {
+      meargeArray.push(sortArr1[arr1Index]);
+      arr1Index++;
+    }
+
+    console.log(meargeArray);
+  }
+}
+
+meargeSortedArray(array1, array2);
