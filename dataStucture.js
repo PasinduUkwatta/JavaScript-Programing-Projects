2c32688b5705f1a0a4b889fee40903688db323ba
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-02-08 13:56:42 +0530

    Add files via upload

---
+++ dataStucture.js
@@ -0,0 +1,24 @@
+// console.log("hello data structure");
+
+// console.log(Math.pow(6, 100));
+
+// const strings = ["a", "b", "c", "d"];
+
+// console.log(strings);
+
+// console.log(strings[2]);
+
+// //push --> this is conctant time
+// strings.push("e"); //o(1)
+
+// console.log(strings);
+
+// strings.pop();
+// strings.pop(); //o(1)
+
+// //unshift
+// strings.unshift("x"); //o(n)
+
+// strings.splice(2, 0, "alien");
+
+// console.log(strings);
