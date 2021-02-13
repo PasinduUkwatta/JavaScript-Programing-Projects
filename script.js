2728562971db204650c251a6ac8dfee0c31fde3e
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-02-13 19:33:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,230 +1,90 @@
-// 'use strict';
+'use strict';
 
-// function calcAge(birthYear) {
-//   const age = 2021 - birthYear;
-//   printAge();
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-//   function printAge() {
-//     let output = `${firstName}, you are ${age} years old & born in ${birthYear} `;
-//     console.log(output);
-//   }
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
-//   if (birthYear >= 1981 && birthYear <= 1996) {
-//     const firstName = 'deshan';
-//     const str = `you are a millanal, ${firstName}`;
-//     console.log(str);
-//     console.log(firstName);
-
-//     function add(a, b) {
-//       console.log(a + b);
-//       return a + b;
-//     }
-//   }
-
-//   return age;
-// }
-
-// const firstName = 'pasindu';
-// calcAge(1996);
-
-// console.log(firstName);
-
-// console.log(me);
-// // console.log(job);
-// // console.log(year);
-
-// var me = 'pasindu';
-// let job = 'student';
-// const year = 1996;
-
-// console.log(addDecl(5, 6));
-
-// // console.log(addExp(4, 6));
-
-// // console.log(addArrow(7, 8));
-
-// function addDecl(a, b) {
-//   return a + b;
-// }
-
-// var addExp = function (a, b) {
-//   return a + b;
-// };
-
-// var addArrow = (a, b) => a + b;
-
-// //example
-// console.log(numProduct);
-
-// if (!numProduct) {
-//   deleteShoppingCart();
-// }
-
-// var numProduct = 10;
-
-// function deleteShoppingCart() {
-//   console.log('All products deleted');
-// }
-
-// var x = 1;
-// let y = 2;
-
-// const z = 3;
-
-// console.log(x === window.x);
-// console.log(y === window.y);
-// console.log(z === window.z);
-// console.log(window.x);
-
-// console.log(this);
-
-// const calcAge1 = function (birthYear) {
-//   console.log(2021 - birthYear);
-//   console.log(this);
-// };
-
-// calcAge1(1996);
-
-// const calcAgeArrow = birthYear => {
-//   console.log(2021 - birthYear);
-//   console.log(this);
-// };
-
-// calcAgeArrow(1996);
-
-// const pasinduTdu = {
-//   year: 1996,
-//   calcAge2: function () {
-//     console.log(this);
-//     console.log(2021 - this.year);
-//   },
-// };
-
-// pasinduTdu.calcAge2();
-
-// const matilda = {
-//   year: 2017,
-// };
-
-// matilda.calcAge = pasinduTdu.calcAge2;
-// matilda.calcAge();
-
-// const f = pasinduTdu.calcAge2;
-// f();
-
-// var firstName = 'thiwanka';
-
-// //this is object literal
-// const pasinduTdu = {
-//   year: 1996,
-//   firstName: 'pasindu',
-//   calcAge2: function () {
-//     // console.log(this);
-//     console.log(2021 - this.year);
-
-//     //solution 1
-//     //   const self = this; //self or that
-//     //   const isMillenial = function () {
-//     //     console.log(self);
-//     //     console.log(self.year >= 1981 && self.year <= 1996);
-//     //   };
-//     //   isMillenial();
-//     // },
-
-//     //solution 2
-//     const isMillenial = () => {
-//       console.log(this);
-//       console.log(this.year >= 1981 && this.year <= 1996);
-//     };
-//     isMillenial();
-//   },
-
-//   greet: function () {
-//     console.log(`hey ${this.firstName}`);
-//   },
-// };
-
-// pasinduTdu.greet(); //arrow function does not have any this keyword
-
-// //in here arrow fucntion acces the global scopr varibale -->var firstname
-// console.log(this.firstName);
-// console.log(window);
-
-// //Arguments keyword
-// // var addExp = function (a, b) {
-// //   console.log(arguments);
-// //   return a + b;
-// // };
-
-// // addExp(2, 5);
-// // addExp(2, 5, 8, 12);
-// // var addArrow = (a, b) => {
-// //   console.log(arguments);
-// //   return a + b;
-// // };
-
-// // //arguments keywords is not support in arrow fucntions
-// // addArrow(2, 5);
-
-// let age = 30;
-// let oldAge = age;
-
-// age = 31;
-
-// console.log(age);
-// console.log(oldAge);
-
-// const me = {
-//   name: 'pasindu',
-//   age: 24,
-// };
-
-// const friend = me;
-// console.log(me);
-// friend.age = 25;
-// friend.lastname = 'deshan';
-
-// console.log(me);
-// console.log(friend);
-
-// let lastName = 'thiwanka';
-// let oldLastName = lastName;
-
-// lastName = 'deshan';
-
-// console.log(lastName, oldLastName);
-
-// const shan = {
-//   firstName: 'thiwanka',
-//   lastName: 'deshan',
-//   age: 25,
-// };
-
-// const marriedShan = shan;
-
-// marriedShan.lastName = 'ukwatta';
-
-// console.log(shan);
-// console.log(marriedShan);
-
-// //we cannot change the value in the stack
-// //becuse the stack address cannot chnage
-// // marriedShan = {};
-
-//copying objects
-
-const shan2 = {
-  firstName: 'thiwanka',
-  lastName: 'deshan',
-  age: 25,
-  marks: [34, 45, 23, 2, 35, 28],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
 };
 
-const shanCopy = Object.assign({}, shan2);
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
 
-shanCopy.lastName = 'pasi';
-shanCopy.marks.push(48);
-shanCopy.marks.push(98);
+// const [x, y, z] = arr;
 
-console.log(shanCopy);
-console.log(shan2);
+// console.log(a, b, c);
+// console.log(x, y, z);
+// console.log(arr);
+
+// //if we need to pass elements we should use empty space
+// let [main, , secondry] = restaurant.categories;
+// console.log(main, secondry);
+
+// const temp = main;
+// main = secondry;
+// secondry = temp;
+// console.log(main, secondry);
+
+// // in this method we does not need any temp variable
+// [main, secondry] = [secondry, main];
+
+// console.log(main, secondry);
+
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// //nested destucturing
+// const nested = [2, 4, [5, 6]];
+
+// // const [i, , j] = nested;
+
+// // console.log(i, j);
+
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// //default values
+// const [p = 2, q = 1, r = 1] = [8, 9];
+
+// console.log(p, q, r);
+
+//destructing the  object
+
+// const { name, openingHours, categories } = restaurant;
+
+// console.log(name, openingHours, categories);
+
+// let a = 111;
+// let b = 999;
+
+// const obj = { a: 23, b: 7, c: 14 };
+// ({ a, b } = obj);
+
+// console.log(a, b);
+
+// const {
+//   fri: { open, close },
+// } = openingHours;
+// console.log(open, close);
