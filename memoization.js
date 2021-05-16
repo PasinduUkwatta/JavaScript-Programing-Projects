f0c0993d3849b0ea0ce3a5c48aae28de6a2f3734
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-05-16 13:04:20 +0530

    memoization update

--- memoization.js
+++ memoization.js
@@ -59,3 +59,16 @@ function memoizedFibonacci() {
 const fastFib = memoizedFibonacci();
 
 fastFib(80);
+
+//---------------------------------------
+
+function fib2(n) {
+  let answer = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    answer.push(answer[i - 2] + answer[i - 1]);
+  }
+
+  return answer.pop();
+}
+
+console.log(fib2(10));
