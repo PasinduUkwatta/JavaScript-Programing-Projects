8f20ef980725dd94b33432afe028c4e3261ba862
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-05-31 08:21:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-import {combineReducers} from "redux"
-import {reducer as formReducer} from "redux-form";
+import {combineReducers} from 'redux'
 import authReducer from "./authReducer";
-import streamReducers from "./streamReducers";
 
 export default combineReducers({
-    auth :authReducer,
-    form:formReducer,
-    streams:streamReducers
+   auth :authReducer
 })
