d4f11959be67bd799feef3c7f3b64c31651ab400
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-06-19 13:37:08 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,63 +1,11 @@
-import React from "react";
-import CardList from "../components/CardList";
-import SearchBox from "../components/SearchBox";
-import Scroll from "../components/Scroll";
+import React from "react"
+import PostList from "./PostList";
 
-
-class App extends React.Component {
-
-    constructor() {
-        super();
-
-        this.state = {
-            robots: [],
-            searchField: ""
-
-        }
-    }
-
-    componentDidMount() {
-      fetch("https://jsonplaceholder.typicode.com/users").then(responce=>{
-            return  responce.json()
-        }).then(users=>{
-            this.setState({robots:users})
-        })
-
-    }
-
-    onSearchChange = (event) => {
-        this.setState({searchField: event.target.value})
-
-    }
-
-
-    render() {
-        const{robots,searchField} =this.state
-
-        const filterRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField.toLowerCase())
-        })
-        console.log(filterRobots)
-        if(!robots.length){
-            return <h1>Loading...</h1>
-        }
-        else {
-            return (
-                <div className="tc ">
-                    <h1>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <CardList robots={filterRobots}/>
-
-                    </Scroll>
-                </div>
-
-            )
-        }
-
-    }
-
-
+const App =()=>{
+    return(
+        <div className="ui container">
+            <PostList/>
+        </div>
+    )
 }
-
 export default App
