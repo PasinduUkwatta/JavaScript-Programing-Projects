02167e41d005c45269f850a8d0965b3a4c55c9e4
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-06-23 11:21:27 +0530

    Robo friends Redux

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-import React from "react";
-import ReactDOM from 'react-dom'
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider,connect} from "react-redux";
+import {createStore,applyMiddleware,combineReducers} from "redux";
+import './index.css';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+import {searchRobots,requestRobots} from "./containers/reducers";
+import {createLogger} from "redux-logger/src";
+import thunk from "redux-thunk";
+import thunkMiddleware from "redux-thunk";
 
+const logger =createLogger()
+const rootReducer =combineReducers({searchRobots,requestRobots})
+const store =createStore(rootReducer,applyMiddleware(thunkMiddleware,logger))
 
-ReactDOM.render(<App/>,document.querySelector("#root"))
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
+registerServiceWorker();
