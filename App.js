e9ecdb8f9a4cb789ec33c6448c42535f05755f77
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-06-28 13:48:01 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import React from "react"
-import PostList from "./PostList";
+import React, {useState, useEffect} from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
 
-const App =()=>{
-    return(
-        <div className="ui container">
-            <PostList/>
-        </div>
-    )
+function App() {
+
+    const [robots, setRobots] = useState([])
+    const [searchField, setSearchField] = useState("")
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(responce => responce.json())
+            .then(users => {
+                setRobots(users)
+            })
+        console.log(count)
+    }, [count])
+
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value)
+    }
+
+
+    const filterRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+    console.log(filterRobots)
+    if (!robots.length) {
+        return <h1>Loading...</h1>
+    } else {
+        return (
+            <div className="tc ">
+                <h1>RoboFriends</h1>
+                <button onClick={()=>setCount(count+1)}>Click Me!</button>
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList robots={filterRobots}/>
+
+                    </ErrorBoundry>
+
+                </Scroll>
+            </div>
+
+        )
+    }
+
+
 }
+
 export default App
