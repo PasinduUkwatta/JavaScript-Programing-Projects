f785a5010b0eb75f8b2ca23ded34d8d8dacbcad2
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-07-08 18:46:17 +0530

    functions

--- script.js
+++ script.js
@@ -1,110 +1,323 @@
-'use strict';
+// 'use strict';
+// // const bookings = [];
+// // const createBooking = function (flightNum, numPassangers = 1, price = 199) {
+// //   //ES5
+// //   // numPassangers = numPassangers || 1;
+// //   // price = price || 199;
 
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
+// //   const booking = {
+// //     flightNum,
+// //     numPassangers,
+// //     price,
+// //   };
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// //   console.log(booking);
+// //   bookings.push(booking);
+// // };
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
+// // createBooking('LH123');
+// // createBooking('LH1234', 3, 350);
+// // createBooking('LH1234', undefined, 350);
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-let highscore = 0;
+// // const flightNumber = 'LH567';
 
-const displayMessage = function (message) {
-  document.querySelector('.message').textContent = message;
-};
+// // const pasindu = {
+// //   name: 'pasindu',
+// //   passport: 3445235,
+// // };
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+// // const checkIn = function (flightNum, passanger) {
+// //   flightNum = 'LH9458';
+// //   passanger.name = 'Mr.' + passanger.name;
 
-  // When there is no input
-  if (!guess) {
-    // document.querySelector('.message').textContent = '⛔️ No number!';
-    displayMessage('⛔️ No number!');
+// //   // if (passanger.passport === 3445235) {
+// //   //   alert('check in');
+// //   // } else {
+// //   //   alert('wrong passport');
+// //   // }
+// // };
 
-    // When player wins
-  } else if (guess === secretNumber) {
-    // document.querySelector('.message').textContent = '🎉 Correct Number!';
-    displayMessage('🎉 Correct Number!');
-    document.querySelector('.number').textContent = secretNumber;
+// // checkIn(flightNumber, pasindu);
+// // console.log(flightNumber);
+// // console.log(pasindu);
 
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
+// // const newPassport = function (person) {
+// //   person.passport = Math.trunc(Math.random() * 100000);
+// // };
 
-    if (score > highscore) {
-      highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
-    }
+// // // newPassport(pasindu);
 
-    // When guess is wrong
-  } else if (guess !== secretNumber) {
-    if (score > 1) {
-      // document.querySelector('.message').textContent =
-      // guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
-      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      // document.querySelector('.message').textContent = '💥 You lost the game!';
-      displayMessage('💥 You lost the game!');
-      document.querySelector('.score').textContent = 0;
-    }
-  }
+// // // checkIn(flightNumber, pasindu);
 
-  //   // When guess is too high
-  // } else if (guess > secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = '📈 Too high!';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = '💥 You lost the game!';
-  //     document.querySelector('.score').textContent = 0;
-  //   }
+// // // console.log(pasindu);
 
-  //   // When guess is too low
-  // } else if (guess < secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = '📉 Too low!';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = '💥 You lost the game!';
-  //     document.querySelector('.score').textContent = 0;
-  //   }
-  // }
-});
+// // const oneWord = function (str) {
+// //   return str.replace(/ /g, '').toLowerCase();
+// // };
 
-document.querySelector('.again').addEventListener('click', function () {
-  score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
+// // const upperFirstWord = function (str) {
+// //   const [firstWord, ...others] = str.split(' ');
 
-  // document.querySelector('.message').textContent = 'Start guessing...';
-  displayMessage('Start guessing...');
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').value = '';
+// //   return [firstWord.toUpperCase(), ...others].join(' ');
+// // };
 
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
-});
+// // //higer order function
+// // const transformer = function (str, fn) {
+// //   console.log(`Original String :${str}`);
+// //   console.log(`Transformed String :${fn(str)}`);
 
-///////////////////////////////////////
-// Coding Challenge #1
+// //   console.log(`Transforemd by :${fn.name} `);
+// // };
 
-/* 
-Implement a game rest functionality, so that the player can make a new guess! Here is how:
+// // transformer('JavaScript is the Best !', upperFirstWord);
+// // transformer('JavaScript is the Best !', oneWord);
 
-1. Select the element with the 'again' class and attach a click event handler
-2. In the handler function, restore initial values of the score and secretNumber variables
-3. Restore the initial conditions of the message, number, score and guess input field
-4. Also restore the original background color (#222) and number width (15rem)
+// // const high5 = function () {
+// //   console.log('🤞  🏳 ');
+// // };
 
-GOOD LUCK 😀
-*/
+// // document.body.addEventListener('click', high5);
+
+// // ['pasindu', 'john', 'amla', 'kamal'].forEach(high5);
+
+// // // const greet = function (greeting) {
+// // //   return function (name) {
+// // //     console.log(`${greeting} ${name}`);
+// // //   };
+// // // };
+
+// // //using arrow function
+// // const greet = greeting => name => console.log(`${greeting} ${name}`);
+
+// // const greeterHay = greet('Hey');
+
+// // greeterHay('Pasindu');
+// // greeterHay('John');
+
+// // greet('hello')('Kamal');
+// // greet('Hi')('Amal');
+
+// // const lufthansa = {
+// //   airline: 'Lufthanaha',
+// //   iataCode: 'LH',
+// //   bookings: [],
+// //   book(flightNum, name) {
+// //     console.log(
+// //       `${name} booked as seat on a ${this.airline} flight ${this.iataCode}${flightNum}`
+// //     );
+
+// //     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+// //   },
+// // };
+
+// // lufthansa.book(239, 'Pasindu Thiwanka');
+// // lufthansa.book(324, 'Deshan');
+// // lufthansa.book(327, 'Amla ');
+// // console.log(lufthansa);
+
+// // const eurowings = {
+// //   airline: 'Euro Wings',
+// //   iataCode: 'EW',
+// //   bookings: [],
+// // };
+
+// // const book = lufthansa.book;
+
+// // // //not work
+// // // book(23,"Sarah")
+
+// // book.call(eurowings, 23, 'Sarah');
+
+// // console.log(eurowings);
+
+// // book.call(lufthansa, 3453, 'Mary Willoms');
+
+// // console.log(lufthansa);
+
+// // const swiss = {
+// //   airline: 'Swiss Airlines',
+// //   iataCode: 'SW',
+// //   bookings: [],
+// // };
+
+// // book.call(swiss, 432, 'Alexa');
+// // console.log(swiss);
+
+// // //Apply method
+
+// // const flightData = [832, 'George'];
+
+// // book.apply(swiss, flightData);
+
+// // console.log(swiss);
+
+// // //bind method
+// // // book.call(eurowings, 23, 'Sarah');
+
+// // const bookEW = book.bind(eurowings);
+// // const bookLH = book.bind(eurowings);
+// // const bookSW = book.bind(eurowings);
+
+// // bookEW(100, 'Stevan');
+// // bookLH(100, 'Stevan');
+
+// // console.log(eurowings);
+
+// // const bookEW23 = book.bind(eurowings, 23);
+// // bookEW23('Pasindu');
+// // bookEW23('deshan');
+
+// // console.log(eurowings);
+
+// // //with event listenrs
+// // lufthansa.planes = 300;
+
+// // lufthansa.buyPlane = function () {
+// //   console.log(this);
+// //   this.planes++;
+// //   console.log(this.planes);
+// // };
+
+// // document
+// //   .querySelector('.buy')
+// //   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// // //partial application
+// // const addTax = (rate, value) => value + value * rate;
+
+// // console.log(addTax(0.1, 200));
+
+// // const addVAT = addTax.bind(null, 0.23);
+
+// // console.log(addVAT(176));
+
+// // const VAT = function (vat) {
+// //   return function (tax) {
+// //     return (vat = vat + vat * tax);
+// //   };
+// // };
+
+// // console.log(VAT(10)(100));
+
+// // function VAT2(vat) {
+// //   return function (tax) {
+// //     return (vat = vat + vat * tax);
+// //   };
+// // }
+
+// // console.log(VAT2(19)(200));
+
+// // TEST DATA 1: [5, 2, 3];
+// // TEST DATA 2: [1, 5, 3, 9, 6, 1];
+
+// // const poll = {
+// //   question: 'What is your favourite programming language?',
+// //   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+// //   // This generates [0, 0, 0, 0]. More in the next section 😃
+// //   answers: new Array(4).fill(0),
+// //   registerAnswer() {
+// //     const answer = Number(
+// //     prompt(
+// //       `${this.question}\n${this.options.join('\n')}\n(write the option number)`
+// //     );
+// //     )
+// //     console.log(answer);
+// // typeof answer === 'number' &&
+// //   answer < this.answer.length &&
+// //   this.answers[answer]++;
+
+// // console.log(this.answers);
+// //     }
+// //   }
+
+// // const runOnce = function () {
+// //   console.log('can run only one time');
+// // };
+
+// // runOnce();
+
+// // //imideiatly ivoke fuctions IIFE
+// // (function () {
+// //   console.log('This will never run again');
+// // })();
+
+// // (() => console.log('THIS WILL NOT RUN AGAIN ->ARROW FUNCTION'))();
+
+// // {
+// //   // const isPrivate = 23;
+// //   var notPrivate = 24;
+// //   // let letPrivate = 25;
+// // }
+
+// // // console.log(isPrivate);
+// // console.log(notPrivate);
+// // // console.log(letPrivate);
+
+// const secureBooking = function () {
+//   let passangerCount = 0;
+
+//   return function () {
+//     passangerCount++;
+//     console.log(`${passangerCount} Passangers`);
+//   };
+// };
+
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+// let f;
+
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 70;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// f();
+// console.dir(f);
+
+// //reasign by h
+// h();
+// f();
+// console.dir(f);
+
+// const boardPassengers = function (n, wait) {
+//   const perGroup = n / 3;
+
+//   setTimeout(function () {
+//     console.log(`we are now boarding all ${n} passangers`);
+//     console.log(`there are 3 groups ,each with ${perGroup} passangers`);
+//   }, wait * 1000);
+//   console.log(`will start boarding in ${wait} seconds`);
+// };
+
+// setTimeout(function () {
+//   console.log('Timer');
+// }, 1500);
+
+// boardPassengers(180, 3);
+
+// const perGroup = 1000;
+
+// (function () {
+//   const header = document.querySelector('h1');
+//   header.style.color = 'red';
+//   document.querySelector('body').addEventListener('click', function () {
+//     header.style.color = 'blue';
+//   });
+// })();
