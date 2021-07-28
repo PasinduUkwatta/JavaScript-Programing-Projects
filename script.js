ef9c9d463763e35a57b36ee91c0e067266ba99dd
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-07-28 18:43:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,69 @@
-const googleDatabase =[
-    'cats.com',
-    'souprecipes.com',
-    'flowers.com',
-    'animals.com',
-    'carpictures.com',
-    'cricket.com',
-    'oldcarnames',
-    'newcarnames',
-    'toyotacarnames',
-    'carbmwnames',
-    'carbmwimages'
-]
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
 
-const googleSearch =(searchInput,db)=>{
-    const matches =db.filter(website =>{
-        return website.includes(searchInput)
-    })
-    return matches.length>5? matches.slice(0,5) :matches
+/*
+const x = 23;
+
+if (x == 23) {
+  console.log("Hello world");
 }
 
-// console.log(googleSearch("car",googleDatabase))
+console.log(x + 10);
+console.log(x + 10);
+console.log(x + 10);
+console.log(x + 10);
+console.log(x + 100);
 
-module.exports=googleSearch
+const temptatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+const newTemp = [];
+
+for (let i = 0; i <= temptatures.length - 1; i++) {
+  if (temptatures[i] !== "error") {
+    console.log(temptatures[i]);
+    newTemp.push(temptatures[i]);
+  }
+}
+
+console.log(newTemp);
+
+const maxTemp = Math.max(...newTemp);
+const minTemp = Math.min(...newTemp);
+
+console.log(maxTemp);
+console.log(minTemp);
+console.log(`Teamprature  Amplitutde is ${maxTemp - minTemp}`);
+
+let tempAmp = [];
+
+for (let i = 0; i <= newTemp.length - 1; i++) {
+  tempAmp.push(maxTemp - newTemp[i]);
+}
+
+console.log(tempAmp);
+*/
+
+/*
+console.log("pasindu");
+console.warn("pasindu");
+console.error("pasindu");
+console.table("pasindu");
+*/
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+const sentence = ["..."];
+
+function printForecast(arr) {
+  for (let i = 0; i <= arr.length - 1; i++) {
+    sentence.push(`${arr[i]} in ${i + 1} days...`);
+  }
+}
+
+//printForecast(data1);
+printForecast(data2);
+console.log(sentence.toString());
+console.log(sentence.join(""));
+
+var testarray = new Array("a", "b", "c");
+console.log(testarray.join(""));
