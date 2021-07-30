2b0258be8525308161c06254efa4f3bc22e0c51a
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-07-30 20:16:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,69 +1,110 @@
-// Remember, we're gonna use strict mode in all scripts now!
-"use strict";
+'use strict';
 
 /*
-const x = 23;
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
 
-if (x == 23) {
-  console.log("Hello world");
-}
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
 
-console.log(x + 10);
-console.log(x + 10);
-console.log(x + 10);
-console.log(x + 10);
-console.log(x + 100);
-
-const temptatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
-
-const newTemp = [];
-
-for (let i = 0; i <= temptatures.length - 1; i++) {
-  if (temptatures[i] !== "error") {
-    console.log(temptatures[i]);
-    newTemp.push(temptatures[i]);
-  }
-}
-
-console.log(newTemp);
-
-const maxTemp = Math.max(...newTemp);
-const minTemp = Math.min(...newTemp);
-
-console.log(maxTemp);
-console.log(minTemp);
-console.log(`Teamprature  Amplitutde is ${maxTemp - minTemp}`);
-
-let tempAmp = [];
-
-for (let i = 0; i <= newTemp.length - 1; i++) {
-  tempAmp.push(maxTemp - newTemp[i]);
-}
-
-console.log(tempAmp);
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
 */
 
-/*
-console.log("pasindu");
-console.warn("pasindu");
-console.error("pasindu");
-console.table("pasindu");
-*/
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
 
-const data1 = [17, 21, 23];
-const data2 = [12, 5, -5, 0, 4];
-const sentence = ["..."];
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
-function printForecast(arr) {
-  for (let i = 0; i <= arr.length - 1; i++) {
-    sentence.push(`${arr[i]} in ${i + 1} days...`);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    // document.querySelector('.message').textContent = '⛔️ No number!';
+    displayMessage('⛔️ No number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    // document.querySelector('.message').textContent = '🎉 Correct Number!';
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      // document.querySelector('.message').textContent =
+      // guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      // document.querySelector('.message').textContent = '💥 You lost the game!';
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
   }
-}
 
-//printForecast(data1);
-printForecast(data2);
-console.log(sentence.toString());
-console.log(sentence.join(""));
+  //   // When guess is too high
+  // } else if (guess > secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📈 Too high!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '💥 You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
 
-var testarray = new Array("a", "b", "c");
-console.log(testarray.join(""));
+  //   // When guess is too low
+  // } else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📉 Too low!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '💥 You lost the game!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
