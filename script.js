8452e4ca50c877dc66624da27dc3753840c50d79
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-08-26 12:22:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,98 +1,69 @@
-'use strict';
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
 
-// Selecting elements
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+/*
+const x = 23;
 
-const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
+if (x == 23) {
+  console.log("Hello world");
+}
 
-let scores, currentScore, activePlayer, playing;
+console.log(x + 10);
+console.log(x + 10);
+console.log(x + 10);
+console.log(x + 10);
+console.log(x + 100);
 
-// Starting conditions
-const init = function () {
-  scores = [0, 0];
-  currentScore = 0;
-  activePlayer = 0;
-  playing = true;
+const temptatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
+const newTemp = [];
 
-  diceEl.classList.add('hidden');
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
-};
-init();
-
-const switchPlayer = function () {
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  currentScore = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
-};
-
-// Rolling dice functionality
-btnRoll.addEventListener('click', function () {
-  if (playing) {
-    // 1. Generating a random dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
-
-    // 2. Display dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
-
-    // 3. Check for rolled 1
-    if (dice !== 1) {
-      // Add dice to current score
-      currentScore += dice;
-      document.getElementById(
-        `current--${activePlayer}`
-      ).textContent = currentScore;
-    } else {
-      // Switch to next player
-      switchPlayer();
-    }
+for (let i = 0; i <= temptatures.length - 1; i++) {
+  if (temptatures[i] !== "error") {
+    console.log(temptatures[i]);
+    newTemp.push(temptatures[i]);
   }
-});
+}
 
-btnHold.addEventListener('click', function () {
-  if (playing) {
-    // 1. Add current score to active player's score
-    scores[activePlayer] += currentScore;
-    // scores[1] = scores[1] + currentScore
+console.log(newTemp);
 
-    document.getElementById(`score--${activePlayer}`).textContent =
-      scores[activePlayer];
+const maxTemp = Math.max(...newTemp);
+const minTemp = Math.min(...newTemp);
 
-    // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 100) {
-      // Finish the game
-      playing = false;
-      diceEl.classList.add('hidden');
+console.log(maxTemp);
+console.log(minTemp);
+console.log(`Teamprature  Amplitutde is ${maxTemp - minTemp}`);
 
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.add('player--winner');
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.remove('player--active');
-    } else {
-      // Switch to the next player
-      switchPlayer();
-    }
+let tempAmp = [];
+
+for (let i = 0; i <= newTemp.length - 1; i++) {
+  tempAmp.push(maxTemp - newTemp[i]);
+}
+
+console.log(tempAmp);
+*/
+
+/*
+console.log("pasindu");
+console.warn("pasindu");
+console.error("pasindu");
+console.table("pasindu");
+*/
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+const sentence = ["..."];
+
+function printForecast(arr) {
+  for (let i = 0; i <= arr.length - 1; i++) {
+    sentence.push(`${arr[i]} in ${i + 1} days...`);
   }
-});
+}
 
-btnNew.addEventListener('click', init);
+//printForecast(data1);
+printForecast(data2);
+console.log(sentence.toString());
+console.log(sentence.join(""));
+
+var testarray = new Array("a", "b", "c");
+console.log(testarray.join(""));
