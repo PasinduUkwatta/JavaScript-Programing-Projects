d1a15ff457ad120f0f76ec34b1471eb3547a54c3
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-11-09 18:05:44 +0530

    Add files via upload

---
+++ ResourceList.js
@@ -0,0 +1,72 @@
+import React,{useState,useEffect} from "react";
+import axios from "axios"
+
+const ResourceList=({resource}) =>{
+
+    const [resources,setResources] =useState([])
+
+
+    const fetchResource = async ()=> {
+       const response= await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+        setResources(response.data)
+        console.log(response.data[0])
+        window.count = Object.keys(response.data[0]).length;
+        console.log(window.count);
+        renderList()
+
+
+    }
+
+   const renderList=()=>{
+       if(window.count===4){
+           return(
+               <div>
+                   <ul>
+                       {resources.map(record=><li key={record.id}>{record.title}</li> )}
+                   </ul>
+
+               </div>
+           )
+       }
+
+       if(window.count===8){
+           return(
+               <div>
+                   <ul>
+                       {resources.map(record=><li key={record.id}>{record.name}</li> )}
+                   </ul>
+
+               </div>
+           )
+       }
+    }
+
+    useEffect(()=>{
+        fetchResource(resource)
+    },[resource])
+if(resources.length<100){
+    return(
+        <div>
+            <ul>
+                {resources.map(record=><li key={record.id}>{record.name}</li> )}
+            </ul>
+
+        </div>
+    )
+}
+else {
+    return(
+        <div>
+            <ul>
+                {resources.map(record=><li key={record.id}>{record.title}</li> )}
+            </ul>
+
+        </div>
+    )
+}
+
+
+
+}
+
+export default ResourceList
