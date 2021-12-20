eb416e0cadbb598643744252b59c0f64997185cc
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-12-20 09:33:04 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,47 +1,12 @@
-import React, { useState } from "react";
-
-import MoviesList from "./components/MoviesList";
-import "./App.css";
+import React from "react";
+import BackwardCounter from "./components/BackwardCounter";
+import ForwardCounter from "./components/ForwardCounter";
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  // const dummyMovies = [
-  //   {
-  //     id: 1,
-  //     title: "Some Dummy Movie",
-  //     openingText: "This is the opening text of the movie",
-  //     releaseDate: "2021-05-18",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Some Dummy Movie 2",
-  //     openingText: "This is the second opening text of the movie",
-  //     releaseDate: "2021-05-19",
-  //   },
-  // ];
-
-  async function fetchMovieHandler() {
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releasedDate: movieData.released_date,
-      };
-    });
-    setMovies(transformedMovies);
-  }
-
   return (
     <React.Fragment>
-      <section>
-        <button onClick={fetchMovieHandler}>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={movies} />
-      </section>
+      <ForwardCounter />
+      <BackwardCounter />
     </React.Fragment>
   );
 }
